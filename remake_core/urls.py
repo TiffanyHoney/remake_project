4a1601713f183a2b52fa58679ef6aa826f8058cd
TiffanyHoney <stayhn0835@gmail.com>
2025-07-15 19:46:37 +0900

    settings.py 2단계 수정 반영

--- remake_core/urls.py
+++ remake_core/urls.py
@@ -1,33 +1,20 @@
-"""
-URL configuration for remake_core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
-
-# 이 두 줄 반드시 추가
+# 반드시 추가
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # 🔽 로그인/로그아웃 등 인증 관련 URL 추가
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # 게시판 앱
     path('', include('board.urls')),
-   
 ]
 
-# 이 부분 반드시 추가
+# 개발 모드일 때만 미디어 파일 서빙
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
